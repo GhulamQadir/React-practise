76a13f4a7d834d7f28ff8748bee6baa605c9e895
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-19 13:38:29 -0700

    explored basic class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React</h1>
-      </header>
 
-    </div >
-  );
+class App extends Component {
+  render() {
+    let myFullName = {
+      firstName: "Ghulam Qadir ",
+      lastName: "Sakaria"
+    }
+
+    let myArr = [1, 2, 3, 4, 5]
+
+    let todos = [
+      { title: "React" },
+      { title: "JavaScript" },
+      { title: "CSS" },
+      { title: "HTML" }
+    ]
+
+    return (
+      <div>
+        {/* Basic Code */}
+        <h1>Hi, {`${myFullName.firstName}${myFullName.lastName}`}</h1>
+        <h2>{myArr}</h2>
+
+
+
+
+        {/* Rendering Todos Array Data using Map*/}
+        <div>
+          {todos.map((todo, index) => {
+            return <p key={index}>{`${index + 1}: ${todo.title}`}</p>
+
+          })}
+        </div>
+        <br />
+      </div>
+    )
+  }
 }
 
 export default App;

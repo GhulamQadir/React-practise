fbe534b9c1d2a3653c733a174a5bdd7768cf2c79
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-22 08:52:50 -0700

    header navbar

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -6,16 +6,17 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <nav>
-                    <h1 className="logo">Header</h1>
-                    <div className="nav_links_div">
-                        <a className="nav_links" href="#">Home</a>
-                        <a className="nav_links" href="#">About</a>
-                        <a className="nav_links" href="#">Products</a>
-                        <a className="nav_links" href="#">Contact</a>
-                    </div>
-                </nav>
-
+                <div className="navbar_div">
+                    <nav className="myNav">
+                        <h1 className="logo">Header</h1>
+                        <div className="nav_links_div">
+                            <a className="nav_links" href="#">Home</a>
+                            <a className="nav_links" href="#">About</a>
+                            <a className="nav_links" href="#">Products</a>
+                            <a className="nav_links" href="#">Contact</a>
+                        </div>
+                    </nav>
+                </div>
             </div>
         )
     }

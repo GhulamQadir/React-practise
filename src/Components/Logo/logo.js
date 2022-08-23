5a305b15f084aaf6e68a572445d3e38787c9956b
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-23 09:58:29 -0700

    updated changes

--- src/Components/Logo/logo.js
+++ src/Components/Logo/logo.js
@@ -5,10 +5,8 @@ import LogoImg from '../../Images/sample-logo.png'
 class Logo extends Component {
     render() {
         return (
-            <div>
-                <div style={{ display: 'inline' }}>
-                    <img src={LogoImg} style={{ display: 'inline', width: 100, height: 40 }} />
-                </div>
+            <div style={{ display: 'inline' }}>
+                <img src={LogoImg} style={{ display: 'inline', width: 100, height: 40 }} />
             </div>
         )
     }
